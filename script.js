9725eb2516e0d03ca60999af443eefdb2f2e7354
Tayo <70311822+ekosetiawan55@users.noreply.github.com>
2026-03-18 13:01:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function openInvitation() {
-  document.getElementById("cover").style.display = "none";
-  document.getElementById("content").style.display = "block";
+  const flap = document.querySelector(".flap");
+  flap.style.transform = "rotateX(180deg)";
+
+  setTimeout(() => {
+    document.getElementById("envelope").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  }, 1000);
 }
 
+/* Countdown */
 const eventDate = new Date("Oct 10, 2026 10:00:00").getTime();
 
 setInterval(function () {
@@ -13,10 +19,27 @@ setInterval(function () {
   document.getElementById("countdown").innerHTML = days + " hari lagi";
 }, 1000);
 
-// Ambil nama tamu dari URL ?to=Nama
+/* Nama tamu dari URL */
 const params = new URLSearchParams(window.location.search);
 const name = params.get("to");
 
 if (name) {
   document.getElementById("guestName").innerText = name;
 }
+
+/* Hujan bunga */
+function createPetal() {
+  const petal = document.createElement("div");
+  petal.classList.add("petal");
+
+  petal.style.left = Math.random() * window.innerWidth + "px";
+  petal.style.animationDuration = (3 + Math.random() * 5) + "s";
+
+  document.getElementById("petals").appendChild(petal);
+
+  setTimeout(() => {
+    petal.remove();
+  }, 8000);
+}
+
+setInterval(createPetal, 300);
